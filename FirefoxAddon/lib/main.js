26fd92166212c38e27661a3645d86cf91063caab
LAbare <florian_fr@orange.fr>
2015-07-23 19:18:38 +0200

    Retour aux sources : site perso plutôt que Rawgit

--- FirefoxAddon/lib/main.js
+++ FirefoxAddon/lib/main.js
@@ -2,5 +2,5 @@ var pageMod = require("sdk/page-mod");
  
 pageMod.PageMod({
 	include: /https?:\/\/mush\.(vg|twinoid\.(com|es))\/?#?$/,
-	contentScript: 'var MM = document.createElement("script"); MM.src = "http://github.com/LAbare/MushMobile/raw/master/MushMobile.user.js"; document.head.appendChild(MM);',
+	contentScript: 'var MM = document.createElement("script"); MM.src = "http://labare.alwaysdata.net/MushMobile/MushMobile.user.js"; document.head.appendChild(MM);',
 });
