2e9dfc96927b61041c4d8cc9881b7af23be37591
LAbare <florian_fr@orange.fr>
2015-08-07 23:11:17 +0200

    Mise à jour source dans l'addon FF et page de présentation

--- FirefoxAddon/lib/main.js
+++ FirefoxAddon/lib/main.js
@@ -2,5 +2,5 @@ var pageMod = require("sdk/page-mod");
  
 pageMod.PageMod({
 	include: /https?:\/\/mush\.(vg|twinoid\.(com|es))\/?#?$/,
-	contentScript: 'var MM = document.createElement("script"); MM.src = "http://labare.alwaysdata.net/MushMobile/MushMobile.js"; document.head.appendChild(MM);',
+	contentScript: 'var MM = document.createElement("script"); MM.src = "http://labare.github.io/MushMobile/MushMobile.js"; document.head.appendChild(MM);',
 });
